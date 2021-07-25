60b6cfe6fb0bde55357d1b4cc609608d09651e2b
Fernando Guerrero <devrrior@gmail.com>
2021-07-25 00:35:12 -0500

    test(orderObject): add test to OrderObject

--- core/cart/tests.py
+++ core/cart/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from core.cart.models import Order
+
 
 # Create your tests here.
+class OrderTests(TestCase):
+    def setUp(self):
+        Order.objects.create(total=-1)
+
+    def test_new_order(self):
+        order = Order.objects.all().first()
+        self.assertEquals(order.total, float(-1))
