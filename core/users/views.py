480e5251f3fa5d37d34a8bb5a2f837e734075ade
Fernando Guerrero <devrrior@gmail.com>
2021-07-27 20:48:08 -0500

    refactor(users/views): improve code

--- core/users/views.py
+++ core/users/views.py
@@ -1,11 +1,6 @@
-from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from django.contrib import messages
-from django.core.mail import EmailMessage
-from django.conf import settings
-from django.template.loader import render_to_string
-from django.views import View
 
 from django_email_verification import send_email
 
@@ -21,12 +16,12 @@ from core.users.forms import RegisterForm
 class UserCreateView(CreateView):
     model = CustomUser
     form_class = RegisterForm
-    template_name = 'registration/signup.html'
-    success_url = reverse_lazy('login')
+    template_name = "registration/signup.html"
+    success_url = reverse_lazy("login")
 
     def form_valid(self, form):
         new_user = form.save()
-        messages.success(self.request, 'Account Created successfully')
+        messages.success(self.request, "Account Created successfully")
 
         returnVal = super(UserCreateView, self).form_valid(form)
 
